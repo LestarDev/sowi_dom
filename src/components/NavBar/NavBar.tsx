916a218dbb372a97e1f48a03b03c7a447bac2bce
LestarDev <m.borkowski.2005@gmail.com>
2024-04-25 22:39:53 +0200

    add colors

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,15 +1,25 @@
 import useProfile from "../../hooks/useProfile"
 import { CgProfile } from "react-icons/cg";
 import { GiLevelFourAdvanced } from "react-icons/gi";
+import './NavBar.css'
 
 const NavBar = () => {
 
     const profile = useProfile();
 
    return <nav aria-label="nav bar">
-    <ul>
-        <li><CgProfile />{profile.nick}</li>
-        <li><GiLevelFourAdvanced />{profile.przelicznik(profile.lvl)}</li>
+    <ul className="nav-ul">
+        <li className="nav-el"><CgProfile className="profileIcon" />{profile.nick}</li>
+        <li className="nav-el">
+            <div>
+                <div>
+                    <GiLevelFourAdvanced />{profile.przelicznik(profile.lvl)}
+                </div>
+                <div>
+                    
+                </div>
+            </div>
+        </li>
     </ul>
    </nav> 
 }
