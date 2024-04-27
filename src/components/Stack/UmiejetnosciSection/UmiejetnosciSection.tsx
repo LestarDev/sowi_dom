9b8173480d515e5dd43e4443f6639f9aa2028330
LestarDev <m.borkowski.2005@gmail.com>
2024-04-28 00:22:34 +0200

    it works in some way

--- src/components/Stack/UmiejetnosciSection/UmiejetnosciSection.tsx
+++ src/components/Stack/UmiejetnosciSection/UmiejetnosciSection.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import getMainLink, { getUmiejetnosci } from "../../../private/apiData"
 import useProfile from "../../../hooks/useProfile"
 
@@ -11,22 +11,32 @@ const UmiejetnosciSection = () => {
         value: number
     }
 
-    const emptyUmiejetnoscTab: umiejetnoscType[] = [];
+    const emptyUmiejetnoscTab: umiejetnoscType[] = [{name:'',value:0}];
 
     const [tabUmiejki, setTabUmiejki] = useState(emptyUmiejetnoscTab);
 
-
+    const refDiv = useRef<HTMLDivElement>(null);
 
     useEffect(()=>{
         fetch(getMainLink(true)+getUmiejetnosci+'id='+profile.idUzytkownika).then(response=>response.json()).then((data: any)=>{
             
-            for(let i=1; i<(data[0]+3); i+=2){
-                const preperTab: umiejetnoscType[] = [...tabUmiejki,{name: data[i],value:data[i+1]}];
-                console.log('Preper Tab: ',preperTab);
-                setTabUmiejki(oldArray=>[...oldArray,{name: data[i], value: data[i+1]}]);
-                console.log(i,data[i],data[i+1]);
-                
+            const preperTab: umiejetnoscType[] = [];
+
+            if(refDiv.current){
+                refDiv.current.innerHTML="";
             }
+
+            for(let i=1; i<(data[0]+3); i+=2){
+
+                const preUmiejka: umiejetnoscType = {name: data[i], value: data[i+1]};
+
+                console.log(i,data[i],data[i+1]);
+                if(refDiv.current){
+                    refDiv.current.innerHTML+=preUmiejka.name+' : '+preUmiejka.value+'<br> ';
+                }
+            }
+
+            setTabUmiejki(oldArray=>[...oldArray, ...preperTab]);
 
             
             //const thisManyUmiejetnosci = 
@@ -35,8 +45,7 @@ const UmiejetnosciSection = () => {
         })
     },[])
 
-    return <div>
-        {}
+    return <div ref={refDiv}>
     </div>
 }
 
