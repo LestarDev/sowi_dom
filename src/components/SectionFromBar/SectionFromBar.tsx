c601ec62070e5c91b7a6bb9a4f63ce75c6c2bc0c
LestarDev <m.borkowski.2005@gmail.com>
2024-04-28 15:50:22 +0200

    dziala+-

--- src/components/SectionFromBar/SectionFromBar.tsx
+++ src/components/SectionFromBar/SectionFromBar.tsx
@@ -1,11 +1,13 @@
 import useProfile from "../../hooks/useProfile"
 import UmiejetnosciSection from "../Stack/UmiejetnosciSection/UmiejetnosciSection";
+import ZdolnosciSection from "../Stack/ZdolnosciSection/ZdolnosciSection";
 
 const SectionFromBar = () => {
     const profile = useProfile();
 
     return <div>
         {profile.wybranyTyp=='Umiejetnosci'? <UmiejetnosciSection /> : ''}
+        {profile.wybranyTyp=='Zdolnosci' ? <ZdolnosciSection /> : ''}
     </div>
 }
 
--- src/components/Stack/ZdolnosciSection/ZdolnosciSection.tsx
+++ src/components/Stack/ZdolnosciSection/ZdolnosciSection.tsx
@@ -1,12 +1,41 @@
-import { useEffect } from "react"
+import { useEffect, useRef } from "react"
+import getMainLink, { getZdolnosciScript } from "../../../private/apiData"
+import useProfile from "../../../hooks/useProfile";
+import { IoIosLink } from "react-icons/io";
 
 const ZdolnosciSection = () => {
 
+    const profile = useProfile();
+
+    const refDiv = useRef<HTMLDivElement>(null);
+
+    type zdolnoscType = {
+        nazwa: string, 
+        czyPolaczone: boolean
+    }
+
     useEffect(()=>{
+        fetch(getMainLink(true)+getZdolnosciScript+"id="+profile.idUzytkownika).then(response=>response.json()).then((data: any)=>{
+
+            if(refDiv.current){
+                refDiv.current.innerHTML="";
+            }
+
+            for(let i=1; i<(data[0]+1); i+=2){
+                console.log('Zdolnosc: ',data[i], data[i+1]);
+                const preperZdolnosc: zdolnoscType = {nazwa: data[i], czyPolaczone: (data[i+1]==1)}
+                const divToPush = document.createElement("div");
+                const spanToPush = document.createElement("span");
+                spanToPush.innerHTML=preperZdolnosc.nazwa+"<IoIosLink />";
+                divToPush.appendChild(spanToPush);
+                refDiv.current?.appendChild(divToPush);
+            }
+
+        })
     },[])
 
     return <div>
-
+        <div ref={refDiv}></div>
     </div>
 }
 
