0ba4e1efec818a56328165ce4c334b73d85fdb26
LestarDev <m.borkowski.2005@gmail.com>
2024-05-05 20:34:44 +0200

    add console log in shop

--- src/pages/Shop/Shop.tsx
+++ src/pages/Shop/Shop.tsx
@@ -19,6 +19,8 @@ const Shop = () => {
 
     const [messageToShop, setMessageToShop] = useState(emptyMessageShop);
 
+    console.log((2*(profile.przelicznik(profile.Niezlomnosc+1,true) as number)),"<=",profile.przeliczLvl(profile.lvl,false,true) as number);
+
     return <div>
         <NavBar></NavBar>
         <OwlShopTitle></OwlShopTitle>
@@ -28,6 +30,7 @@ const Shop = () => {
             <OwlModule nazwa="Zdobycie k4+1 Szczescia" koszt={1} type={profile.sowieMonety >= 5 ? "kupuj" : "brak monet"} fun={setMessageToShop}/>
             <OwlModule nazwa={"Zdobycie +0.1 lvl [na "+profile.przeliczLvl(profile.lvl*1 + 1)+"]"} koszt={1} type={profile.sowieMonety >= 5 ? "kupuj" : "brak monet"} fun={setMessageToShop}/>
             <OwlModule nazwa="Rozwin 3 umiejetnosci [do x.4]" koszt={1} type={profile.sowieMonety >= 5 ? "kupuj" : "brak monet"} fun={setMessageToShop} />
+            
             {(2*(profile.przelicznik(profile.Cialo+1,true) as number))<=(profile.przeliczLvl(profile.lvl,false,true) as number) ? <OwlModule nazwa={'Podnies Cialo o 0.1 [na '+profile.przelicznik(profile.Cialo*1+1)+']'} koszt={1} type={profile.sowieMonety >= 5 ? "kupuj" : "brak monet"} fun={setMessageToShop} /> : ''}
             {(2*(profile.przelicznik(profile.Umysl+1,true) as number))<=(profile.przeliczLvl(profile.lvl,false,true) as number) ? <OwlModule nazwa={'Podnies Umysl o 0.1 [na '+profile.przelicznik(profile.Umysl*1+1)+']'} koszt={1} type={profile.sowieMonety >= 5 ? "kupuj" : "brak monet"} fun={setMessageToShop} /> : ''}
             {(2*(profile.przelicznik(profile.Zrecznosc+1,true) as number))<=(profile.przeliczLvl(profile.lvl,false,true) as number) ? <OwlModule nazwa={'Podnies Zrecznosc o 0.1 [na '+profile.przelicznik(profile.Zrecznosc*1+1)+']'} koszt={1} type={profile.sowieMonety >= 5 ? "kupuj" : "brak monet"} fun={setMessageToShop} /> : ''}
