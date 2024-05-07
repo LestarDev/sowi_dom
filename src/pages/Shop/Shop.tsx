51cd21924c20b973929b297f914026f936dbdd9a
LestarDev <m.borkowski.2005@gmail.com>
2024-05-07 23:59:34 +0200

    restore error

--- src/pages/Shop/Shop.tsx
+++ src/pages/Shop/Shop.tsx
@@ -66,8 +66,9 @@ const Shop = () => {
         }} name={singleUmiejka.name} id={singleUmiejka.name} /></label>)
     })
 
-    if(podnies3UmiejkiTab.length<3) console.log('x');
-    //setCountSelected3umiejki(3-podnies3UmiejkiTab.length)
+    if(podnies3UmiejkiTab.length<3){
+        setCountSelected3umiejki(3-podnies3UmiejkiTab.length)
+    }
 
 
     return <div>
