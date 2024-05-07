4199d353921aedd3af3b5d445d961b557aeacdc5
LestarDev <m.borkowski.2005@gmail.com>
2024-05-07 23:53:20 +0200

    add start value for 3 umiejki owl shop

--- src/pages/Shop/Shop.tsx
+++ src/pages/Shop/Shop.tsx
@@ -66,6 +66,8 @@ const Shop = () => {
         }} name={singleUmiejka.name} id={singleUmiejka.name} /></label>)
     })
 
+    if(podnies3UmiejkiTab.length<3) setCountSelected3umiejki(3-podnies3UmiejkiTab.length);
+
 
     return <div>
         <NavBar></NavBar>
