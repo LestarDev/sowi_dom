9321d39976dfd2d8eb40bda993a1365bbcb04837
LestarDev <m.borkowski.2005@gmail.com>
2024-05-08 00:17:18 +0200

    mayby this way

--- src/pages/Shop/Shop.tsx
+++ src/pages/Shop/Shop.tsx
@@ -73,9 +73,12 @@ const Shop = () => {
         }} name={singleUmiejka.name} id={singleUmiejka.name} /></label>)
     })
 
-    if(podnies3UmiejkiTab.length<3) setCountSelected3umiejki(3-podnies3UmiejkiTab.length)
+    // if(podnies3UmiejkiTab.length<3) setCountSelected3umiejki(3-podnies3UmiejkiTab.length)
 
     return <div>
+        {
+            podnies3UmiejkiTab.length<3 ? (setCountSelected3umiejki(3-podnies3UmiejkiTab.length), '') : ''
+        }
         <NavBar></NavBar>
         <OwlShopTitle></OwlShopTitle>
         <div className="owlShop">
