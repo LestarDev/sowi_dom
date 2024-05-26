2210c09cbc98341a787639dc20b240bc1057a987
Lestardev <m.borkowski.2005@gmail.com>
2024-05-26 13:56:24 +0200

    redo search umiejka

--- src/components/Stack/UmiejetnosciSection/UmiejetnosciSection.tsx
+++ src/components/Stack/UmiejetnosciSection/UmiejetnosciSection.tsx
@@ -11,26 +11,31 @@ const UmiejetnosciSection = () => {
 
     const profile = useProfile();
 
-        
+    const emptyUmiejka: umiejetnoscType = {id: 0, name: '', type: 0, value: 0};    
 
-    const [windowUmiejkaData, setWindowUmiejkaData] = useState({value: 0, cecha: 0,nazwa:''});
+    const [windowUmiejkaData, setWindowUmiejkaData] = useState(emptyUmiejka);
 
     const [rnText, setRnText] = useState('');
 
     const [showSearch, setShowSearch] = useState(false);
 
-    const refDiv = useRef<HTMLDivElement>(null);
+    const emptyDivElement = <div className="section"></div>;
+    const [divElement, setDivElement] = useState(emptyDivElement);
+
+    // const refDiv = useRef<HTMLDivElement>(null);
     const refDivOpenWindow = useRef<HTMLDivElement>(null);
     const refInputSearch = useRef<HTMLInputElement>(null);
 
     const cechyNazwy = ["Umysl", "Cialo", "Zrecznosc","Niezlomnosc", "Intuicja", "Urok"];
 
     const openWindow = (danaUmiejka: umiejetnoscType) => {
-        setWindowUmiejkaData({value: danaUmiejka.value, cecha: danaUmiejka.type, nazwa: danaUmiejka.name});
+        setWindowUmiejkaData(danaUmiejka);
+        // setWindowUmiejkaData({value: danaUmiejka.value, cecha: danaUmiejka.type, nazwa: danaUmiejka.name});
     }
 
     const closeWindow = () => {
-        setWindowUmiejkaData({value:0, cecha: 0, nazwa:''});
+        setWindowUmiejkaData(emptyUmiejka);
+        // setWindowUmiejkaData({value:0, cecha: 0, nazwa:''});
         setModifyRoll(0);
     }
 
@@ -42,10 +47,7 @@ const UmiejetnosciSection = () => {
         const onScroll = () => setOffset(window.scrollY);
         fetch(getMainLink(isStackBlitz)+getUmiejetnosciScript+'id='+profile.idUzytkownika).then(response=>response.json()).then((data: any)=>{
             
-
-            if(refDiv.current){
-                refDiv.current.innerHTML="";
-            }
+            setDivElement(emptyDivElement)
 
             // console.log("Umiejetnosci:",data);
 
@@ -55,27 +57,16 @@ const UmiejetnosciSection = () => {
 
                 const preUmiejka: umiejetnoscType = {name: data[i], value: Number(data[i+1]),type: Number(data[i+2]), id: data[i+3]};
 
-                //console.log(i,data[i],data[i+1],data[i+2]);
-                if(refDiv.current){
-                    //const divEl = createElement("div",{className: "singleUmiejka"},createElement("span",preUmiejka.name));
-                    const divEl = document.createElement("div");
-                    const firstSpan = document.createElement("span");
-                    const secondSpan = document.createElement("span");
-                    const buttonOpen = document.createElement("button");
-                    divEl.className="singleUmiejka";
-                    firstSpan.innerHTML=preUmiejka.name;
-                    secondSpan.innerHTML=profile.przelicznik(preUmiejka.value) as string;
-                    buttonOpen.innerHTML="Info";
-                    buttonOpen.onclick= function(){
-                        openWindow(preUmiejka);
-                    }
-                    divEl.appendChild(firstSpan);
-                    divEl.appendChild(secondSpan);
-                    divEl.appendChild(buttonOpen);
-                    refDiv.current.appendChild(divEl);
-                    
-                    //refDiv.current.innerHTML+=preUmiejka.name+' : '+preUmiejka.value+'<br> ';
-                }
+
+                setDivElement(prevEl=><div  className="UmiejetnosciSection">
+                    {prevEl.props.children}
+                    <div>
+                        <span>{preUmiejka.name}</span>
+                        <span>{profile.przelicznik(preUmiejka.value)}</span>
+                        <button onClick={()=>{openWindow(preUmiejka)}}>Info</button>
+                    </div>
+                </div>);
+
             }
 
 
@@ -116,13 +107,15 @@ const UmiejetnosciSection = () => {
         </div>
         {
             !showSearch ? <>
-            <div ref={refDiv} className="section"></div>
-            <div ref={refDivOpenWindow} className={windowUmiejkaData.cecha==0 ? "window" : "windowShowed"} style={{transform: "translateY("+offset+"px)"}}>
+            {
+                divElement
+            }
+            <div ref={refDivOpenWindow} className={windowUmiejkaData.type==0 ? "window" : "windowShowed"} style={{transform: "translateY("+offset+"px)"}}>
                 <div className="umiejkaBox">
-                    <h2>{windowUmiejkaData.nazwa}</h2>
+                    <h2>{windowUmiejkaData.name}</h2>
                     <span>Umiejetnosc{modifyRoll ? `${modifyRoll>0 ? '+' : ''}${modifyRoll}` : ''}: {profile.przelicznik(windowUmiejkaData.value+modifyRoll)}</span>
-                    <span>Cecha: {cechyNazwy[windowUmiejkaData.cecha-1]} {'['}{profile.przelicznik(profile.getCeche(windowUmiejkaData.cecha))}{']'}</span>
-                    <span>Rzucasz: <b>{profile.zlaczoneKostki(windowUmiejkaData.value+modifyRoll, profile.getCeche(windowUmiejkaData.cecha))}</b></span>
+                    <span>Cecha: {cechyNazwy[windowUmiejkaData.type-1]} {'['}{profile.przelicznik(profile.getCeche(windowUmiejkaData.type))}{']'}</span>
+                    <span>Rzucasz: <b>{profile.zlaczoneKostki(windowUmiejkaData.value+modifyRoll, profile.getCeche(windowUmiejkaData.type))}</b></span>
                     <div>
                         <button onClick={()=>setModifyRoll(prevV=>prevV-1)}>-</button>
                         <button onClick={closeWindow}>x</button>
