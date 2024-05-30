de29c8801ac26c02af7f6053147159b956c0e136
LestarDev <m.borkowski.2005@gmail.com>
2024-05-30 11:53:55 +0200

    redo this as JSX element at admin page to more debug

--- src/pages/Admin/Admin.tsx
+++ src/pages/Admin/Admin.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 import useProfile from "../../hooks/useProfile"
 import getMainLink, { AdminGetIDsScript } from "../../private/apiData";
 import { isStackBlitz } from "../../shared/config/isStackBlitz";
+import { GiLevelFourAdvanced, GiOwl } from "react-icons/gi";
 
 type uzytkownikType = {
     id: number,
@@ -14,17 +15,29 @@ const Admin = () => {
 
     const profile = useProfile();
 
-    const emptyUzytkownicy: uzytkownikType[] = []
-
-    const [uzytkownicyToLogin, setUzytkownicyToLogin] = useState(emptyUzytkownicy);
+    const [uzytkownicyToLogin, setUzytkownicyToLogin] = useState(<div></div>);
 
     useEffect(()=>{
         fetch(getMainLink(isStackBlitz)+AdminGetIDsScript).then(response=>response.json()).then((data: any)=>{
             console.log(data);
             for(let i=1; i<(Number(data[0])*4); i+=4){
-                const uzytkownikToAdd = {id: Number(data[i]), nick: data[i+1], lvl: Number(data[i+2]), sowieMonety: Number(data[i+3])}
-                console.log(i,uzytkownikToAdd)
-                setUzytkownicyToLogin(prevTab=>[...prevTab, uzytkownikToAdd]);
+                const uzytkownikToAdd: uzytkownikType = {id: Number(data[i]), nick: data[i+1], lvl: Number(data[i+2]), sowieMonety: Number(data[i+3])}
+                // console.log(i,uzytkownikToAdd)
+                setUzytkownicyToLogin(prevEl=><div>
+                    {prevEl.props.children}
+                    <div className="singleUzytkownik">
+                        <span>{uzytkownikToAdd.id}</span>
+                        <span>{uzytkownikToAdd.nick}</span>
+                        <div>
+                            <span>{uzytkownikToAdd.lvl}</span>
+                            <GiLevelFourAdvanced />
+                        </div>
+                        <div>
+                            <span>{uzytkownikToAdd.sowieMonety}</span>
+                            <GiOwl />
+                        </div>
+                    </div>
+                </div>);
                 console.log("[Inner for]",uzytkownicyToLogin);
             }
 
@@ -34,7 +47,9 @@ const Admin = () => {
     },[profile.refreshPage])
 
     return <div id="Admin">
-        {/* {uzytkownicyToLogin[0].nick} */}
+        {
+            uzytkownicyToLogin
+        }
     </div>
 }
 
