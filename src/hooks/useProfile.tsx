aa904d1c3c71e69895030e39e431ee56adcf1d6f
LestarDev <m.borkowski.2005@gmail.com>
2024-05-30 10:51:39 +0200

    redo infocechy, trujaca => trujacy

--- src/hooks/useProfile.tsx
+++ src/hooks/useProfile.tsx
@@ -203,7 +203,7 @@ if(val1<=0 || val2<=0) return 'PECH';
     const infoCechy: DiceLevel = {
         "szybka":"Przeciwnik ma -0.1 do Uniku oraz Parowania",
         "podwójny atak": "Jesli udalo Ci sie zaatakowac przeciwnika mozesz go zaatakowac po raz drugi [dla niego to ta sama reakcja]",
-        "trująca (X)": "Przeciwnik po Twoim ataku i na koncu swojej tury wykonuje test odp na trucizny [TESTY]w przeciwnym razie traci k3 HP i dostaje debuff Zatruty (+1) za kazdy nieudany test",
+        "trujący (X)": "Przeciwnik po Twoim ataku i na koncu swojej tury wykonuje test odp na trucizny [TESTY]w przeciwnym razie traci k3 HP i dostaje debuff Zatruty (+1) za kazdy nieudany test",
         "wytrzymała": "Bron jest niesamowidzie wytrzymala i bardzo trudna do zniszczenia",
         "precyzyjna (X)": "Jesli zadasz choc 1HP zaatakowana istota traci dodatkowe [INSERT]HP bez wzgledu na wlasne zdolnosci"
     }
@@ -213,7 +213,7 @@ if(val1<=0 || val2<=0) return 'PECH';
         
         const rangaCechy: string = nameCecha.split('(')[1][0];
 
-        const preperOpis = infoCechy[nameCecha.toLowerCase().split('(')[0]+"(X)"] ?? `Brak ${nameCecha.split('(')[0]+"(X)"}`;
+        const preperOpis = infoCechy[nameCecha.toLowerCase().split('(')[0]+"(X)"] ?? `Brak ${nameCecha.toLowerCase().split('(')[0]+"(X)"}`;
 
         // const preperTab = preperOpis.split("[TESTY]");
 
